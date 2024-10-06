f2bf5e2a45aa2b913017becb8ec6a4ec78b01d75
Prateek Passi <passi264@gmail.com>
2024-10-06 16:12:25 -0400

    Updated vercel.json and App.py for deployment

--- App.py
+++ App.py
@@ -2,23 +2,26 @@ from flask import Flask, request, jsonify
 import requests
 import os
 
-
 app = Flask(__name__)
 api_key = os.getenv('EXCHANGE_RATE_API_KEY')
 
-@app.route('/', methods=["POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
-    data = request.get_json()
-    source_currency = data["queryResult"]["parameters"]["unit-currency"]["currency"]
-    amount = data["queryResult"]["parameters"]["unit-currency"]["amount"]
-    target_currency = data["queryResult"]["parameters"]["currency-name"]
-    cf = fetch_conversion_factor(source_currency, target_currency)
-    final_amount = amount * cf
-    final_amount = round(final_amount, 2)
-    response = {
-        "fulfillmentText": "{} {} is {} {}".format(amount, source_currency, final_amount, target_currency)
-    }
-    return jsonify(response)
+    if request.method == "GET":
+        return "Welcome to the Currency Converter API!"
+
+    elif request.method == "POST":
+        data = request.get_json()
+        source_currency = data["queryResult"]["parameters"]["unit-currency"]["currency"]
+        amount = data["queryResult"]["parameters"]["unit-currency"]["amount"]
+        target_currency = data["queryResult"]["parameters"]["currency-name"]
+        cf = fetch_conversion_factor(source_currency, target_currency)
+        final_amount = amount * cf
+        final_amount = round(final_amount, 2)
+        response = {
+            "fulfillmentText": "{} {} is {} {}".format(amount, source_currency, final_amount, target_currency)
+        }
+        return jsonify(response)
 
 def fetch_conversion_factor(source, target):
     url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/{source}/{target}"
@@ -26,8 +29,17 @@ def fetch_conversion_factor(source, target):
     respond = response.json()
     return respond["conversion_rate"]
 
+# Handle missing favicon.ico to avoid 404 errors
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
 
 
 
